6cbf2b73649fb83f354becde97a03ebfa716b3fc
antonio <antoniomanzari@gmail.com>
2021-01-08 17:16:50 -0500

    Init commit for Deliverable1

---
+++ Deliverable1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!";
+            Console.WriteLine(message);
+        }
+    }
+}
